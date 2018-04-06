f4bcdf31835bcae5239baae9f9b11a68687cf7f1
CAMILO ANDRES ANZOLA GONZALEZ <ca.anzola@sis.virtual.uniandes.edu.co>
2018-04-06 10:33:27 -0500

    Conexion lista

--- caso2/src/Cliente.java
+++ caso2/src/Cliente.java
@@ -1,10 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Cliente 
+{
 
-public class Cliente {
+	//private final static String IP = ;
+	private final static int PUERTO = 8080;  
+	
+	public static void main(String[] args) 
+	{
+		Socket socket = null;
+		PrintWriter escritor = null;
+		BufferedReader lector = null;
+		try 
+		{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			socket = new Socket(InetAddress.getLocalHost().getHostAddress(), PUERTO);
+			escritor = new PrintWriter(socket.getOutputStream(), true);
+			
+			lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			
+			String fromServer;
+			String fromUser;
+			boolean ejecutar = true;
+			while (ejecutar) 
+			{
+				System.out.print("Escriba el mensaje para enviar:");
+				fromUser = stdIn.readLine();
+				escritor.println(fromUser);
+				if ((fromServer = lector.readLine()) != null) 
+				{
+					System.out.println("Servidor: " + fromServer);
+				}
+			}
+			escritor.close();
+			lector.close();
+			// cierre el socket y la entrada estándar
+			socket.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error " + e.getMessage());
+			System.exit(1);
+		}
 	}
 
 }
